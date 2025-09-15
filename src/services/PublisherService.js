b7f3bec8d4995c8a93b3437d4ddf7ba10f8487cc
Paul Zhang <paulzh@umich.edu>
2025-09-15 17:01:36 -0400

    hotfix for the publisher service

--- src/services/PublisherService.js
+++ src/services/PublisherService.js
@@ -5,31 +5,39 @@ const RABBITMQ_USER_EXCHANGE = process.env.RABBITMQ_USER_EXCHANGE;
 const RABBITMQ_USER_EXCHANGE_TYPE = process.env.RABBITMQ_USER_EXCHANGE_TYPE;
 const RABBITMQ_USER_ROUTING_KEY = process.env.RABBITMQ_USER_ROUTING_KEY;
 
-const RABBITMQ_EMAIL_EXCHANGE = process.env.RABBITMQ_EMAIL_EXCHANGE;
-const RABBITMQ_EMAIL_EXCHANGE_TYPE = process.env.RABBITMQ_EMAIL_EXCHANGE_TYPE;
-const RABBITMQ_EMAIL_ROUTING_KEY = process.env.RABBITMQ_EMAIL_ROUTING_KEY;
+// const RABBITMQ_EMAIL_EXCHANGE = process.env.RABBITMQ_EMAIL_EXCHANGE;
+// const RABBITMQ_EMAIL_EXCHANGE_TYPE = process.env.RABBITMQ_EMAIL_EXCHANGE_TYPE;
+// const RABBITMQ_EMAIL_ROUTING_KEY = process.env.RABBITMQ_EMAIL_ROUTING_KEY;
 
 
 async function publishUserCreated(payload) {
-  console.log("payload", payload)
-  const conn = await amqp.connect(RABBITMQ_URL);
-  const ch = await conn.createChannel();
+    console.log("payload", payload)
+    const conn = await amqp.connect(RABBITMQ_URL);
+    const ch = await conn.createChannel();
 
-  const msg = JSON.stringify(payload);
+    const msg = JSON.stringify(payload);
 
-  await ch.assertExchange(
-    RABBITMQ_USER_EXCHANGE, 
-    RABBITMQ_USER_EXCHANGE_TYPE, 
-    { 
-      durable: true 
-    }
-  );
+    await ch.assertExchange(
+        RABBITMQ_USER_EXCHANGE,
+        RABBITMQ_USER_EXCHANGE_TYPE,
+        {
+            durable: true
+        }
+    );
 
-  ch.publish(RABBITMQ_USER_EXCHANGE, RABBITMQ_USER_ROUTING_KEY, Buffer.from(msg));
-  console.log(" [x] Sent %s", msg);
+    await ch.assertQueue(`${RABBITMQ_USER_EXCHANGE}.${RABBITMQ_USER_ROUTING_KEY}`, { durable: true });
+    await ch.bindQueue(
+        `${RABBITMQ_USER_EXCHANGE}.${RABBITMQ_USER_ROUTING_KEY}`,
+        RABBITMQ_USER_EXCHANGE,
+        RABBITMQ_USER_ROUTING_KEY
+    );
 
-  // await channel.close();
-  await conn.close();
+
+    ch.publish(RABBITMQ_USER_EXCHANGE, RABBITMQ_USER_ROUTING_KEY, Buffer.from(msg));
+    console.log(" [x] Sent %s", msg);
+
+    // await channel.close();
+    // await conn.close();
 }
 
 export default publishUserCreated;
